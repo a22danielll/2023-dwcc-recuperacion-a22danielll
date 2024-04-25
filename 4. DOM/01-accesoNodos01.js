4838f509a2efeae82aa4a83d9bd24c03603a8874
a22danielll <a22danielll@iessanclemente.net>
2024-04-25 11:58:40 +0200

    s

---
+++ 4. DOM/01-accesoNodos01.js
@@ -0,0 +1,6 @@
+let taboa = document.querySelector('#age-table');
+console.log(taboa);
+let label = document.querySelector('#age-table').getElementsByTagName('label');
+console.log(label);
+let td = document.getElementById('age-table').querySelector('td');
+console.log(td);
