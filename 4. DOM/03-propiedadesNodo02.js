a6d8df289a33327ecd7a5847b7b8c40857a583fc
a22danielll <a22danielll@iessanclemente.net>
2024-05-02 11:30:10 +0200

    s

---
+++ 4. DOM/03-propiedadesNodo02.js
@@ -0,0 +1,12 @@
+'use strict';
+console.log('a');
+
+let ligazon = document.querySelectorAll('a').length;
+console.log(ligazon);
+
+console.log('b');
+let ligazon2 = document.querySelectorAll('a');
+
+console.log(ligazon2[ligazon2.length - 2].href);
+
+console.log('c');
