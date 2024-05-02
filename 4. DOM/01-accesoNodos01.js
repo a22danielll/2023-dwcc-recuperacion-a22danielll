72dc2ffe8eaa382c56a84ced4c565eced8ed7a02
a22danielll <a22danielll@iessanclemente.net>
2024-05-02 09:29:59 +0200

    s

--- 4. DOM/01-accesoNodos01.js
+++ 4. DOM/01-accesoNodos01.js
@@ -1,8 +1,25 @@
+console.log('a');
+
 let taboa = document.querySelector('#age-table');
 console.log(taboa);
-let label = document.querySelector('#age-table').getElementsByTagName('label');
-console.log(label);
+console.log('b');
+
+let labels = document.getElementById('age-table').querySelectorAll('label');
+labels.forEach((x) => console.log(x));
+
+console.log('c');
+
 let td = document.getElementById('age-table').querySelector('td');
 console.log(td);
-let search = document.querySelector('name[search]');
+console.log('d');
+
+let search = document.querySelector('form[name="search"]');
 console.log(search);
+
+console.log('e');
+let input = search.querySelector('input');
+console.log(input);
+console.log('f');
+let input2 = document.getElementsByName('search')[0].querySelectorAll('input');
+
+console.log(input2[1]);
