b2e0dfef40ac8c2822cdaf5b4e73d1a21a82ca96
a22danielll <a22danielll@iessanclemente.net>
2024-05-06 18:23:12 +0200

    s

--- 4. DOM/03-propiedadesNodo02.js
+++ 4. DOM/03-propiedadesNodo02.js
@@ -10,3 +10,14 @@ let ligazon2 = document.querySelectorAll('a');
 console.log(ligazon2[ligazon2.length - 2].href);
 
 console.log('c');
+let ligazon3 = document.querySelectorAll('[href="http://proba"]');
+console.log(ligazon3.length);
+
+console.log('d');
+let ligazon4 = document.querySelectorAll('p')[2];
+console.log(ligazon4.querySelectorAll('a').length);
+
+console.log('e');
+for (let i of ligazon3) {
+    i.style.color = 'orange';
+  }
